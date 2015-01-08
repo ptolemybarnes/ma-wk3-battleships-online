fda82bd7ed26828b51f1595888091843ac597847
Ptolemy <ptolemybarnes@gmail.com>
2015-01-08 12:25:56 +0000

    Moved instantiation of Board outside of /boardpage to achieve persistence.

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -3,8 +3,15 @@ require_relative 'ship'
 require_relative 'board'
 require_relative 'cell'
 require_relative 'water'
+require_relative 'game'
 
 class BattleShips < Sinatra::Base
+enable :sessions
+
+game = Game.new
+board = Board.new(Cell)
+board.grid.each {|coord, cell| cell.content = Water.new }
+
   get '/' do
     erb :index
   end
@@ -20,14 +27,11 @@ class BattleShips < Sinatra::Base
   end
 
   get '/boardpage' do
-   @board = Board.new(Cell)
-   @board.grid.each {|coord, cell| cell.content = Water.new }
+   if params[:shot]
+      board.shoot_at(params[:shot].to_sym)
+   end
 
-     if params[:shot]
-      @board.shoot_at(params[:shot].to_sym)
-     end
-
-   @keys = @board.grid.keys
+   @keys = board.grid.keys
    erb :boardpage
   end
   
